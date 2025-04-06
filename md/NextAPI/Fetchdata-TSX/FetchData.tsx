895551688c185ea61b33d9ac11574c712bbc20ac
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-04-06 15:43:51 +0700

    u folders

---
+++ md/NextAPI/Fetchdata-TSX/FetchData.tsx
@@ -0,0 +1,31 @@
+type BlogPost = {
+    id: number;
+    title: string;
+  };
+  
+  export default async function Page() {
+    const data = await fetch('https://api.vercel.app/blog');
+    const posts: BlogPost[] = await data.json();
+  
+    return (
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    );
+  }
+  
+  // export default async function Page() {
+  //   const data = await fetch('https://api.vercel.app/blog');
+  //   const posts = await data.json();
+  
+  //   return (
+  //     <ul>
+  //       {posts.map((post: any) => (
+  //         <li key={post.id}>{post.title}</li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
+  
